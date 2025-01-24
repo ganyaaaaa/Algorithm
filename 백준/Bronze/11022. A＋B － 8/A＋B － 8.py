0a032ff6e7f90132e3e785e5182eb2fd98760f90
gaeun <kimgy5807@gmail.com>
2025-01-24 14:06:09 +0900

    Update A＋B － 8.py

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -4,3 +4,12 @@ T = int(sys.stdin.readline())
 for i in range(1,T+1):
     A, B = map(int,sys.stdin.readline().split())
     print(f"Case #{i}: {A} + {B} = {A+B}")
+
+# 다른 방법
+# import sys
+# input = sys.stdin.readline
+# T = int(input())
+
+# for i in range(1,T+1):
+#     A, B = map(int,input().split())
+#     print(f"Case #{i}: {A} + {B} = {A+B}")
